09ccd37eb456102d4defadc529c6fbafa39c0b3d
Beanfork <beanforkaccess@gmail.com>
2019-08-21 15:06:28 +0530

    Added Util folder

--- src/Models/Comment.js
+++ src/Models/Comment.js
@@ -1,18 +1,18 @@
 createComment = function(postid,email,comment,username){
   // create the object with email,comment,username
 
-  //update the db with the object as fields
+  //update the db-post with the object as fields
 
 }
 
 editComment = function(postid,commentid,newcomment){
   //check for postid and commentid
 
-  //update the comment field with the new comment
+  //update the comment field in db-post with the new comment
 }
 
 deleteComment = function(postid,commentid){
     //check for postid and commentid 
 
-    //delete the comment and then commentid
+    //delete the comment and then commentid from db-post
   }
